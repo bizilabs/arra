479e8ff90fc114a180a59e9b8ca291099b24e0cb
MamboBryan <mambobryan@gmail.com>
2025-11-25 21:48:14 +0300

    chore : adds package file for declaration

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Arra",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "Arra", targets: ["Arra"])
+    ],
+    targets : [
+        .target(name: "Arra", dependencies: []),
+        .testTarget(name: "ArraTests", dependencies: ["Arra"])
+    ]
+)
